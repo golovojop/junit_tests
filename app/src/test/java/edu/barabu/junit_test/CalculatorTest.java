1e4ca76183d31e013e4168d071666d1e5a71c214
ciscoff <ciscoff@mail.ru>
2018-12-02 14:18:13 +0300

    test is ready

--- app/src/test/java/edu/barabu/junit_test/CalculatorTest.java
+++ app/src/test/java/edu/barabu/junit_test/CalculatorTest.java
@@ -1,4 +1,48 @@
 package edu.barabu.junit_test;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
+    Calculator calc;
+    int a;
+    int b;
+
+
+
+    @Before
+    public void setUp(){
+        calc = new Calculator();
+        a = 11;
+        b = 0;
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        int expected = a + b;
+        assertEquals(expected, calc.add(a, b));
+    }
+
+    @Test
+    public void testSub() throws Exception {
+        int expected = a - b;
+        assertEquals(expected, calc.sub(a, b));
+    }
+
+    @Test
+    public void testMul() throws Exception {
+        int expected = a * b;
+        assertEquals(expected, calc.mul(a, b));
+    }
+
+    @Test
+    public void testDiv() throws Exception {
+        int expected = (b == 0) ? 0 : (a / b);
+        assertEquals(expected, calc.div(a, b));
+    }
+
+
+
+
 }
